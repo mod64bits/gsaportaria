333d3c1f52bb9182ab5a3fcd5b145784c76dfb9e
mod64bits <mod64bits@gmail.com>
2021-09-19 00:49:37 -0300

    static

--- gsa/settings.py
+++ gsa/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['gsaportaria.com.br/gsa', 'localhost', '127.0.0.1']
 
 # Application definition
 
