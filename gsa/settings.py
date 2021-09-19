d3e7da22d252f62928f914e18841d5cdfa92bdcf
mod64bits <mod64bits@gmail.com>
2021-09-19 00:04:22 -0300

    static

--- gsa/settings.py
+++ gsa/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "staticfiles"),
+# ]
 #STATIC_ROOT = "/static/"
 MEDIA_URL = "/media/"
 if not DEBUG:
