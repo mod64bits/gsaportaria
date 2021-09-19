8b66b986657f81556ceb8232a57eff38beb7ccff
mod64bits <mod64bits@gmail.com>
2021-09-19 00:51:16 -0300

    static

--- gsa/settings.py
+++ gsa/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['gsaportaria.com.br/gsa', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['gsaportaria.com.br', 'localhost', '127.0.0.1']
 
 # Application definition
 
