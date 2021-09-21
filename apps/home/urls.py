dc93ac132ec5f451f1213896b79a5326992f64f0
mod64bits <mod64bits@gmail.com>
2021-09-20 23:09:29 -0300

    Correção home e tela de contato

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, re_path
 from .views import HomeView
+from apps.contatos.views import contato
 
 
 app_name = 'home'
 
 urlpatterns = [
+    path('contato/', contato),
     path('', HomeView.as_view(), name='home'),
 ]
