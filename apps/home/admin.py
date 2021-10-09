dec195c61c857d878f75ea87ab3eda3048f8b0c1
mod64bits <mod64bits@gmail.com>
2021-10-08 22:53:34 -0300

    app parceiros

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Parceiros
 
-# Register your models here.
+
+class ParceirosAdmin(admin.ModelAdmin):
+    list_filter = ['ativo', 'nome', 'created_at']
+    search_fields = ('titulo',)
+
+
+admin.site.register(Parceiros, ParceirosAdmin)
