ea20f126b91f9a6644f41f76bc3c3a2388aeab02
mod64bits <mod64bits@gmail.com>
2022-09-29 13:38:43 -0300

    template servico detalhe

--- apps/servicos/urls.py
+++ apps/servicos/urls.py
@@ -5,5 +5,5 @@ app_name = 'servicos'
 
 urlpatterns = [
     path('servicos/', ServicosView.as_view(), name='servicos'),
-    path('<uuid:pk>/', ServicoDetalhe.as_view(), name='servico_detalhe'),
+    path('<slug:slug>/', ServicoDetalhe.as_view(), name='servico_detalhe'),
 ]
--- apps/servicos/views.py
+++ apps/servicos/views.py
@@ -5,17 +5,12 @@ from django.views.generic.list import ListView
 
 class ServicoDetalhe(DetailView):
     model = Servico
-    context_object_name = 'ser_detalhe'
     template_name = 'servicos/ServicoDetalhe.html'
 
-    @property
-    def imagens_servicos(self):
-        id = self.kwargs['pk']
-        return id
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['imagens'] = Imagem.objects.filter(servico_id=self.imagens_servicos)
+        # context['imagens'] = Imagem.objects.filter(servico_id=self.imagens_servicos)
         return context
 
 
